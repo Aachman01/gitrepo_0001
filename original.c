67170fe807a89c67a423acc33c691ea4b5fe4947
Aachman <aachman0001@gmail.com>
2024-02-14 11:10:46 +0530

    implemented conditions

--- original.c
+++ original.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
-    printf("Hello world");for(int i=0;i<10;i++){
+    printf("Hello world");int a=5; if (a>3){for(int i=0;i<10;i++){
         printf("%d",i);
-    }
+
+    }}
 }
